557cc3a7b9edba24c3e51990be5d68efe48b63c7
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-04-28 14:48:01 -0300

    💄style: estiliza componente AddTarefa

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
   return (
     <>
       <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-        <div className="w=[500]">
+        <div className="w=[500] space-y-4">
 
           <h1 className="text-3xl text-slate-100 font-bold text-center">
             Gerenciador de Tarefas
