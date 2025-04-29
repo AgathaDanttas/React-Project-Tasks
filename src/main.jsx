9498bc34bbf65a3c6ccec52ee81d1db16b4b5d4e
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-04-29 16:36:48 -0300

    🚀feat: configurar rotas com React Router e adicionar navegação para App e Tarefas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TarefasPages from "./pages/TarefasPages.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/Tarefas",
+    element:<TarefasPages/>,
+  },
+
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
