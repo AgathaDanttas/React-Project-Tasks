55ba8968236348f8853562246f48cbf9da3a74eb
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-05-05 14:42:12 -0300

    💄style: Estilizar a Página de Detalhes da Tarefa

--- src/pages/TarefasPages.jsx
+++ src/pages/TarefasPages.jsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon } from "lucide-react";
 import { useSearchParams } from "react-router-dom";
 
 function TarefasPages() {
@@ -6,9 +7,21 @@ function TarefasPages() {
   const description = searchParams.get("description");
 
   return (
-    <div className="w-screen h-screen bg-slate-500 flex flex-col  p-6 text-white">
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <div className="w-screen h-screen bg-slate-500 p-6">
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative">
+          <button className="absolute left-0 mb-6 text-slate-100">
+            <ChevronLeftIcon/>
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">Detalhes da Tarefa</h1>
+
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md ">
+          <h2 className="text-xl text-slate-700 font-bold">{title}</h2>
+          <p className="text-slate-700">{ description}</p>
+        </div>
+      </div>
     </div>
   );
 }
